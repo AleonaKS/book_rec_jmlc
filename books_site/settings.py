bd16fb38ff0b3f5501eff84d0b37651ed691e902
AleonaKS <challengesim@gmail.com>
2025-07-22 01:59:24 +0400

    update for host render

--- books_site/settings.py
+++ books_site/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#9ss9!ex_*iqc55y&)h0ej2ki5jfh064rhhsviw8w+emwox)*('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["book-rec-jmlc-1.onrender.com"]
+
 
 # CSRF_COOKIE_HTTPONLY = True
 # Application definition
